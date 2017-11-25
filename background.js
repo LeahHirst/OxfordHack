4cfa6c4a1412a1487864678d1aec4a28d3efc686
Adam Hirst <adam@ahirst.com>
2017-11-25 15:39:39 +0000

    Added files

--- background.js
+++ background.js
@@ -6,3 +6,10 @@ function registerVideo(element) {
 
 // Find all videos of the page
 var videos = document.getElementsByTagName("video");
+
+setInterval(function() { togglePause(videos[0]); }, 3000);
+
+function togglePause(element) {
+  if (element.paused) element.play();
+  else element.pause();
+}
